0106a6d8096d690aa4ad20ab44151587bcdeb24d
Rens Saladaga <staleexam19@gmail.com>
2025-10-11 05:00:43 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <RouterView />
+</template>
+
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import { useColorMode } from "@vueuse/core";
+
+
+// Para sa theme switch (dark/light modes)
+useColorMode();
+
+</script>
