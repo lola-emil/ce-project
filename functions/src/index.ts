00a7a062fece0b277e9fc6c27a9fe6597b137bcb
Rens Saladaga <staleexam19@gmail.com>
2025-12-03 03:56:44 +0800

    feat: added basic functionality for activity log

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,32 +1,54 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
 
 import { setGlobalOptions } from "firebase-functions";
 import { onRequest } from "firebase-functions/https";
 import * as logger from "firebase-functions/logger";
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+import { onDocumentCreated, onDocumentUpdated } from "firebase-functions/firestore";
+import { getFirestore } from "firebase-admin/firestore";
+import { initializeApp } from "firebase-admin/app";
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
+
 setGlobalOptions({ maxInstances: 10 });
+initializeApp();
+const db = getFirestore();
 
 
 export const helloWorld = onRequest((_request, response) => {
   logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
+});
+
+
+export const onJobCreated = onDocumentCreated("job_requests/{jobId}", async (event) => {
+  const data = event.data?.data();
+  console.log("Data:", data);
+
+
+  const logEntry = {
+    type: "created",
+    data,
+    timestamp: Date.now()
+  };
+
+  await db.collection("activityLogs").add(logEntry);
+
+  console.log("Activity log created:", logEntry);
+})
+
+export const onJobUpdated = onDocumentUpdated("job_requests/{jobId}", async (event) => {
+  const before = event.data?.before.data();  // QueryDocumentSnapshot → data()
+  const after = event.data?.after.data();
+
+  console.log("Before:", before);
+  console.log("After:", after);
+
+
+  const logEntry = {
+    type: "update",
+    after,
+    timestamp: Date.now()
+  };
+
+  await db.collection("activityLogs").add(logEntry);
+
+  console.log("Activity log created:", logEntry);
 });
